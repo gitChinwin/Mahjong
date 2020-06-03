be0f5b8c5670e6a433fcc52dbfb419ee42ffac26
jam <jamwong994@gmail.com>
2020-06-03 20:23:44 +0800

    sort mj2

--- src/internal/win2.go
+++ src/internal/win2.go
@@ -1,5 +1,7 @@
 package internal
 
+import "log"
+
 /**
 * @Author: Jam Wong
 * @Date: 2020/6/3
@@ -13,5 +15,40 @@ func (pl *Player) Win2() bool {
 		return false
 	}
 
+	tmp := make([]*Tile, 0)
+	copy(tmp, pl.HoldTiles)
+
+	for i := 0; i < len(pl.HoldTiles)-1; i++ {
+		if isPair(pl.HoldTiles[i], pl.HoldTiles[i+1]) {
+			tmp := make([]*Tile, 0)
+			copy(tmp, pl.HoldTiles)
+			remove(tmp, i+1)
+			remove(tmp, i)
+		}
+	}
+
 	return true
+}
+
+func remove(t []*Tile, n int) {
+	if len(t) < 2 {
+		log.Fatal("at least 1 tile.")
+	}
+	t[n] = t[len(t)-1]
+	t[len(t)-1] = nil
+	t = t[:len(t)-1]
+}
+
+func hu(t []*Tile) bool {
+	if len(t) == 0 {
+		return true
+	}
+	if isSequence(t[0], t[1], t[2]) {
+		return hu(t[3:])
+	}
+
+	if isTriplet(t[0], t[1], t[2]) {
+		return hu(t[3:])
+	}
+	return false
 }
